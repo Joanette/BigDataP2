10617335f50c8a1b953ad6e2065c480982e4b67e
Rosario <joanette.rosario@honeywell.com>
2018-12-07 11:17:58 -0400

    testing tweets by taking a print out

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -44,20 +44,20 @@ def read_tweets(access_token, access_secret, consumer_key, consumer_secret):
     # Here we set it to stop after getting 1000 tweets.
     # You don't have to set it to stop, but can continue running
     # the Twitter API to collect data for days or even longer.
-    tweet_count = 1000
+    tweet_count = 10
     for tweet in iterator:
         tweet_count -= 1
         # Twitter Python Tool wraps the data returned by Twitter
         # as a TwitterDictResponse object.
         try:
             # print screen_name and name
-            print("TWEET: ", tweet['user']['screen_name'])
+            print("TWEET: ", tweet['user']['screen_name'], "\n")
             # The command below will do pretty printing for JSON data, try it out
-            print("TWEET JSON: ", tweet['text'])
+            print("TWEET JSON: ", tweet['text'], "\n")
             # This next command, prints the tweet as a string
-            print("CREATED_AT:", tweet['created_at'])
-            print("Favorite count: ", tweet("favorite_count"))
-            print ("TWEETS STRING", str(tweet))
+            print("CREATED_AT:", tweet['created_at'], "\n")
+            print("Favorite count: ", tweet("favorite_count"), "\n")
+           #print ("TWEETS STRING", str(tweet))
         except:
             pass
 
